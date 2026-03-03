1fe5e927bc7a688d25173cb857a766aaa77b7a0e
Manasses <manasseshudson@gmail.com>
2026-03-03 09:43:17 -0300

    add

--- database/database.js
+++ database/database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host : process.env.HOST,
-    port : process.env.PORT_BANCO,
-    user : process.env.USER,
+    host : '168.75.108.235',
+    port : '3306',
+    user : 'gerencie_os',
     password : '#Manasses2025',
-    database : process.env.DATABASE
+    database : 'gerencie_os'
   },debug:false
 });
 
